022f8edfc3f7f6dfceb2d73c40b476a507b0f10e
Bhanuchandar23 <bhanuchandar8991@gamil.com>
2024-05-25 20:29:54 +0530

    createfrist component and understanding vite and react  files

---
+++ basicvite/src/App.jsx
@@ -0,0 +1,13 @@
+import Youtube from './Youtube.jsx'
+
+function App() {
+  return( 
+    <div>
+       <h1> vite react </h1>
+       <h1> vite react </h1>
+       <Youtube/>
+    </div>
+  )
+}
+
+export default App
